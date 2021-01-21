d9e529f9165adddd9df63140b3a8bb8af76f865a
Alvin Zablan <azablan.dev@gmail.com>
2021-01-21 11:31:42 -0500

    h2 12

---
+++ 12_hw.py
@@ -0,0 +1,3 @@
+# leetcode#101 - https://leetcode.com/problems/symmetric-tree/
+# leetcode#111 - https://leetcode.com/problems/minimum-depth-of-binary-tree/
+# leetcode#226 - https://leetcode.com/problems/invert-binary-tree/
