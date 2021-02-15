8fe70b8361a753ee9c7a784aa98e64ccc537e069
tongsalex <tongsalex@gmail.com>
2021-02-15 09:53:37 -0500

    hw

--- 29.py
+++ 29.py
@@ -4,45 +4,55 @@
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        return self.isMatchHelper(s, p)
+        memo = {}
+        return self.isMatchHelper(s, p, memo)
         
-    def isMatchHelper(self, s, p):
-        if not s and not p:
+    def isMatchHelper(self, s, p, memo, i = 0, j = 0):
+        key = (i, j)
+        if key in memo:
+            return memo[key]
+        if i == len(s) and j == len(p):
             return True
-        if s and not p:
+
+        if i < len(s) and j == len(p):
+            return False
+ 
+        if (i == len(s) or j == len(p)) and p[j] != '*':
             return False
 
-        # if not s and p[0] != '*':
-        #     return False
-        
-        if not s or not p:
-            return False
-
-        
-
-        if p[0] == '*':
-            if self.isMatchHelper(s[1:], p[1:]):
-              return True
-
-            if s and self.isMatchHelper(s[1:], p):
-              return True
+        if p[j] == '*':
+            if i == len(s):
+                memo[key] = self.isMatchHelper(s, p, memo, i, j + 1)
+                return memo[key]
+            else:
+                memo[key] = self.isMatchHelper(s, p, memo, i + 1, j) or self.isMatchHelper(s, p, memo, i, j + 1)
+                return memo[key]
             
-        if s[0] == p[0] or p[0] == '?':
-            return self.isMatchHelper(s[1:], p[1:])
+        if s[i] == p[j] or p[j] == '?':
+            memo[key] = self.isMatchHelper(s, p, memo, i + 1, j + 1)
+            return memo[key]
 
+        memo[key] = False
         return False
 
 test = Solution()
-print(test.isMatch("boot", "?oot"))# T
-print(test.isMatch("boot", "?o?t"))# T
-print(test.isMatch("boot", "?ot")) # F
-print(test.isMatch("boot", "?sdsdst"))# F
+print(test.isMatch('aa', 'a')) # F
+print(test.isMatch('a', '')) # F
 
-print(test.isMatch("bsddast", "*sdsdst"))# F
-print(test.isMatch("boot", "b**t"))# T
-print(test.isMatch("apple", "*"))# T
-print(test.isMatch("apple", "apple*"))# T
-print(test.isMatch("x", "*"))# T
-print(test.isMatch("", "*"))# T
-print(test.isMatch("", "*s"))# F
+# print(test.isMatch("", "*")) # T
+
+# print(test.isMatch("boot", "?oot"))# T
+# print(test.isMatch("boot", "?o?t"))# T
+# print(test.isMatch("boot", "?ot")) # F
+# print(test.isMatch("boot", "?sdsdst"))# F
+
+# print(test.isMatch("bsddast", "*sdsdst"))# F
+# print(test.isMatch("boot", "b**t"))# T
+# print(test.isMatch("apple", "*"))# T
+# print(test.isMatch("apple", "apple*"))# T
+# print(test.isMatch("x", "*"))# T
+# print(test.isMatch("", "*"))# T
+# print(test.isMatch("", "*s"))# F
+
+# https://awwapp.com/b/uwyrr2syjyjln/
 
